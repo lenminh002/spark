8095970a74e7e26b2022aff785a85366989fbd76
Minh <lenminh002@gmail.com>
2026-03-15 17:03:54 -0700

    connect backend to frontend

--- app/app.py
+++ app/app.py
@@ -1,13 +1,9 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from chatbot import get_response
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
 
 @app.route("/chat", methods=["POST"])
 def chat():
@@ -15,3 +11,6 @@ def chat():
     user_input = data.get("message")
     response = get_response(user_input)
     return jsonify({"response": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
